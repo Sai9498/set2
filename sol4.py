4c8332aa11e5abc4a7e80205a29d4509e6ae5ae8
Sai9498 <52398738+Sai9498@users.noreply.github.com>
2019-07-04 12:02:46 +0530

    Update sol4.py

--- sol4.py
+++ sol4.py
@@ -1,4 +1,4 @@
-num = list(map(int, input().split()))
-for i in range(num[0]+1, num[-1]):
+numbr = list(map(int, input().split()))
+for i in range(numbr[0]+1, numbr[-1]):
   if i % 2 != 0:
     print(i, end=" ")
